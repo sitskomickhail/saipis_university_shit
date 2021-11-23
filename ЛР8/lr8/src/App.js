5d6314f4a3e030710d5b37d0a11bc231262898ac
Mikhail Sitsko <masitsko@gmail.com>
2021-11-23 14:50:58 +0300

    Updating LR8 styles, creating select input

--- ЛР8/lr8/src/App.js
+++ ЛР8/lr8/src/App.js
@@ -6,13 +6,27 @@ import Form from "./components/formComponent";
 async function saveData(e) {
     e.preventDefault();
 
+    const email = e.target.first_mail.value;
+    const phone = e.target.first_phone.value;
+
+    if(!email.match('^[^@\\s]+@[^@\\s\\.]+\\.[^@\\.\\s]+$')) {
+        alert("Некорректный email адрес!");
+        return;
+    }
+
+    if(!phone.match("^[0-9\\-\\+]{9,15}$")) {
+        alert("Некорректный телефонный номер!");
+        return;
+    }
+
     const postData = {
         name: e.target.first_name.value,
         surname: e.target.first_surname.value,
-        phone: e.target.first_phone.value,
-        mail: e.target.first_mail.value,
+        phone: phone,
+        mail: email,
         sex: e.target.first_man.checked ? "Мужской" : "Женский",
         skills: e.target.first_skills.value,
+        programmingLanguage: e.target.first_programmingLanguage.value,
         communication: [e.target.first_connection_phone.checked, e.target.first_connection_mail.checked]
     };
 
@@ -21,6 +35,8 @@ async function saveData(e) {
     };
 
     axios.post('http://localhost:5000/api/data', postData, {headers});
+
+    alert("Успешно сохранено!");
 }
 
 function App() {
@@ -45,10 +61,12 @@ function App() {
                       disabled={false}
                       buttonText="Сохранить данные"
                       buttonCallback={saveData}
+                      reloadAfterSubmit={false}
                       curiculumObject={null}/>
                 <Form idPrefix="second"
                       disabled={secondAreaAvailability}
                       buttonText="Получить и отобразить данные"
+                      reloadAfterSubmit={true}
                       buttonCallback={getData}
                       curiculumObject={curiculumObject}/>
             </div>
--- ЛР8/lr8/src/components/formComponent.js
+++ ЛР8/lr8/src/components/formComponent.js
@@ -1,75 +1,117 @@
 import React from "react";
+import {useState, useEffect} from 'react';
 
 export default function Form(props) {
+
+    const [sex, setSex] = useState("");
+    const [mailConnection, setMailConnection] = useState(false);
+    const [phoneConnection, setPhoneConnection] = useState(false);
+    const [programmingLanguage, setProgrammingLanguage] = useState("");
+
+    useEffect(() => {
+        if (props.reloadAfterSubmit) {
+            setPhoneConnection(props.curiculumObject === null ? false : props.curiculumObject.communication[1]);
+            setMailConnection(props.curiculumObject === null ? false : props.curiculumObject.communication[0]);
+            setSex(props.curiculumObject === null ? "" : props.curiculumObject.sex);
+            setProgrammingLanguage(props.curiculumObject === null ? "" : props.curiculumObject.programmingLanguage);
+        }
+    }, [props])
+
     return (
         <div>
             <form className="formArea" onSubmit={(e) => props.buttonCallback(e)}>
                 <div>
-                    <label htmlFor={`${props.idPrefix}_name`}>Имя</label>
+                    <label className="helpTagLabel" htmlFor={`${props.idPrefix}_name`}>Имя</label>
                     <div>
                         <input disabled={props.disabled}
                                defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.name}
                                id={`${props.idPrefix}_name`}
-                               type="text"/>
+                               type="text"
+                               className="formValues"/>
                     </div>
                 </div>
                 <div>
-                    <label htmlFor={`${props.idPrefix}_surname`}>Фамилия</label>
-                    <div><input disabled={props.disabled}
-                                defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.surname}
-                                id={`${props.idPrefix}_surname`}
-                                type="text"/></div>
+                    <label className="helpTagLabel" htmlFor={`${props.idPrefix}_surname`}>Фамилия</label>
+                    <div>
+                        <input disabled={props.disabled}
+                               defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.surname}
+                               id={`${props.idPrefix}_surname`}
+                               type="text"
+                               className="formValues"/>
+                    </div>
                 </div>
                 <div>
-                    <label htmlFor={`${props.idPrefix}_phone`}>Номер телефона</label>
+                    <label className="helpTagLabel" htmlFor={`${props.idPrefix}_phone`}>Номер телефона</label>
                     <div>
                         <input disabled={props.disabled}
                                defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.phone}
-                               id={`${props.idPrefix}_phone`} type="text"/>
+                               id={`${props.idPrefix}_phone`} type="text" className="formValues"/>
                     </div>
                 </div>
                 <div>
-                    <label htmlFor={`${props.idPrefix}_mail`}>Email</label>
+                    <label className="helpTagLabel" htmlFor={`${props.idPrefix}_mail`}>Email</label>
                     <div>
                         <input disabled={props.disabled}
                                defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.mail}
-                               id={`${props.idPrefix}_mail`} type="email"/>
+                               id={`${props.idPrefix}_mail`} type="email"
+                               className="formValues"/>
                     </div>
                 </div>
                 <div>
-                    <label>Пол</label>
+                    <label className="helpTagLabel">Пол</label>
                     <div>
                         <input disabled={props.disabled}
-                               defaultChecked={props.curiculumObject == null ? "" : props.curiculumObject.sex === "Мужской"}
+                               onChange={() => setSex("Мужской")}
+                               checked={sex === "Мужской"}
                                type="radio" name="sex" id={`${props.idPrefix}_man`}/>
-                        <label htmlFor={`${props.idPrefix}_man`}>Мужской</label>
+                        <label htmlFor={`${props.idPrefix}_man`} className="formValues">Мужской</label>
                         <input disabled={props.disabled}
-                               defaultChecked={props.curiculumObject == null ? "" : props.curiculumObject.sex === "Женский"}
+                               onChange={() => setSex("Женский")}
+                               checked={sex === "Женский"}
                                type="radio" name="sex" id={`${props.idPrefix}_woman`}/>
-                        <label htmlFor={`${props.idPrefix}_woman`}>Женский</label>
+                        <label htmlFor={`${props.idPrefix}_woman`}
+                               className="formValues">Женский</label>
                     </div>
                 </div>
                 <div>
-                    <label htmlFor={`${props.idPrefix}_skills`}>Ваши ключевые навыки</label>
+                    <label htmlFor={`${props.idPrefix}_programmingLanguage`} className="helpTagLabel">Язык
+                        программирования</label>
+                    <select id={`${props.idPrefix}_programmingLanguage`}
+                            className="formValues"
+                            disabled={props.disabled}>
+                        <option selected={programmingLanguage === "C#"} value="C#">C#</option>
+                        <option selected={programmingLanguage === "Java"} value="Java">Java</option>
+                        <option selected={programmingLanguage === "Ruby"} value="Ruby">Ruby</option>
+                        <option selected={programmingLanguage === "Swift"} value="Swift">Swift</option>
+                        <option selected={programmingLanguage === "C++"} value="C++">C++</option>
+                        <option selected={programmingLanguage === "JavaScript"} value="JavaScript">JavaScript</option>
+                    </select>
+                </div>
+                <div>
+                    <label className="helpTagLabel" htmlFor={`${props.idPrefix}_skills`}>Ваши ключевые навыки</label>
                     <div>
                         <textarea disabled={props.disabled}
                                   defaultValue={props.curiculumObject == null ? "" : props.curiculumObject.skills}
+                                  cols="65"
+                                  className="formValues"
                                   rows="10" id={`${props.idPrefix}_skills`}/>
                     </div>
                 </div>
                 <div>
-                    <label>Выберите удобные средства связи с вами</label>
+                    <label className="helpTagLabel">Выберите удобные средства связи с вами</label>
                     <div>
                         <input disabled={props.disabled} type="checkbox"
-                               defaultChecked={props.curiculumObject == null ? "" : props.curiculumObject.communication[0]}
+                               onChange={() => setPhoneConnection(!phoneConnection)}
+                               checked={phoneConnection}
                                id={`${props.idPrefix}_connection_phone`}
                                name={`${props.idPrefix}_connection_phone`}/>
-                        <label htmlFor={`${props.idPrefix}_connection_phone`}>Через телефон</label>
+                        <label className="formValues" htmlFor={`${props.idPrefix}_connection_phone`}>Через телефон</label>
                         <input disabled={props.disabled} type="checkbox"
-                               defaultChecked={props.curiculumObject == null ? "" : props.curiculumObject.communication[1]}
+                               checked={mailConnection}
+                               onChange={() => setMailConnection(!mailConnection)}
                                id={`${props.idPrefix}_connection_mail`}
                                name={`${props.idPrefix}_connection_mail`}/>
-                        <label htmlFor={`${props.idPrefix}_connection_mail`}>Через почту</label>
+                        <label className="formValues" htmlFor={`${props.idPrefix}_connection_mail`}>Через почту</label>
                     </div>
                 </div>
                 <button type="submit">{props.buttonText}</button>
